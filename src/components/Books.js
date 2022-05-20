b7a1a6a4df71a8d700a3f2ca98d932cbc364eed8
Mustali <74243780+mustali17@users.noreply.github.com>
2022-05-20 11:58:33 +0530

    update

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,9 +4,17 @@ export default function Books({ book }) {
   const { bookname, author, desc } = book;
   return (
     <>
-      <h3>Book Name:{bookname}</h3>
-      <h3>Author Name:{author}</h3>
-      <h3>Description:{desc}</h3>
+      <div className="card" style={{ width: "18rem" }}>
+        <img src="..." class="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{bookname}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">{author}</h6>
+          <p className="card-text">{desc}</p>
+          <a href="#" className="btn btn-primary">
+            Add to cart
+          </a>
+        </div>
+      </div>
     </>
   );
 }
