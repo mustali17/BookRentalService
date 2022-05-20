1200096e275e4c1ac4e9da08159089d3ce4b5abf
Mustali <74243780+mustali17@users.noreply.github.com>
2022-05-20 12:04:23 +0530

    updatte

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,18 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Books({ book }) {
   const { bookname, author, desc } = book;
   return (
     <>
       <div className="card" style={{ width: "18rem" }}>
-        <img src="..." class="card-img-top" alt="..." />
+        <img
+          src="https://m.media-amazon.com/images/I/51-nXsSRfZL.jpg"
+          className="card-img-top"
+          height="220"
+          width="200"
+          alt="..."
+        />
         <div className="card-body">
           <h5 className="card-title">{bookname}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{author}</h6>
           <p className="card-text">{desc}</p>
-          <a href="#" className="btn btn-primary">
+          <Link to="/cart" className="btn btn-primary">
             Add to cart
-          </a>
+          </Link>
         </div>
       </div>
     </>
