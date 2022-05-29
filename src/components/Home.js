72bc1c1f09c44cfbfc7bd628684b6daa4f12e227
Mustali <74243780+mustali17@users.noreply.github.com>
2022-05-29 11:45:43 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,10 +14,10 @@ export default function Home() {
         }}
       >
         <div>
-          <h1 style={{ fontFamily: "cambria", color: "#00539CFF" }}>
+          <h1 style={{ fontFamily: "cambria", color: "white" }}>
             Books on Rent
           </h1>
-          <h3 style={{ fontFamily: "cambria", color: "#00539CFF" }}>
+          <h3 style={{ fontFamily: "cambria", color: "white" }}>
             Books on your doorstep
           </h3>
           <Link to="/books">
@@ -28,13 +28,13 @@ export default function Home() {
           <br />
           <br />
           <br />
-          <h6 style={{ fontFamily: "garamond" }}>
+          <h5 style={{ fontFamily: "garamond" }}>
             If you want to give your books on rent kindly email the details of
             your book at:{" "}
             <a href="mailto:mustalichunawala@gmail.com?subject=Mail from website&body=Hey! Thanks for connecting with us. Please fill out the following details of your book.%0d%0aBook Name:%0d%0aAuthor Name: %0d%0aYour Address: %0d%0aDuration:">
               mustalichunawala@gmail.com
             </a>
-          </h6>
+          </h5>
         </div>
         <img
           src="https://freepngimg.com/thumb/reading/146802-boy-reading-book-png-free-photo.png"
