00202104f73783cc4e74a1799b54936328f1c323
mustali17 <74243780+mustali17@users.noreply.github.com>
2022-07-14 18:25:57 +0530

    update

--- src/components/Book.js
+++ src/components/Book.js
@@ -25,7 +25,7 @@ export default function RecordList() {
  useEffect(() => {
    async function getRecords() {
 
-     const response = await fetch('http://localhost:5000/api/record/');
+     const response = await fetch('/api/record/');
 
  
      if (!response.ok) {
